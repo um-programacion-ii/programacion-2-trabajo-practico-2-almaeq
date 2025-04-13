d4a965bee38b51d4837256e07cff08fdcb46c003
almaeq <alma.quinterosquiroga@gmail.com>
2025-04-12 23:28:52 -0300

    add RecursoDigital and EstadoRecurso

--- DigitalLibrary/src/recursoDigital/RecursoDigital.java
+++ DigitalLibrary/src/recursoDigital/RecursoDigital.java
@@ -2,6 +2,6 @@ package recursoDigital;
 
 public interface RecursoDigital {
     String getIdentificador();
-   //EstadoRecurso getEstado();
-    //void actualizarEstado(EstadoRecurso estado);
+    EstadoRecurso getEstado();
+    void actualizarEstado(EstadoRecurso estado);
 }
