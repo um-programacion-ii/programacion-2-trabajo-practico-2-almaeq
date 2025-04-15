7878715de20b9b730d84fce1c91868b009fab4f9
almaeq <alma.quinterosquiroga@gmail.com>
2025-04-15 10:00:16 -0300

    add interface ServicioNotificaciones

---
+++ DigitalLibrary/src/servicios/ServicioNotificaciones.java
@@ -0,0 +1,7 @@
+package servicios;
+
+public interface ServicioNotificaciones {
+    void enviarNotificacion(String destinatario, String mensaje);
+    void desactivarNotificaciones(String usuario);
+    boolean estaActivo(String usuario); //verificar si el usuario las tiene activadas
+}
